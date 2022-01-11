5a911827944776d1b11465ebffd6443d85fbdd95
Julien Verlaguet <julien.verlaguet@gmail.com>
2022-01-11 09:40:44 -0500

    Moved all the content from website/ to root

--- docs/build.sh
+++ docs/build.sh
@@ -11,4 +11,4 @@ cat docs_header.html >> docs.html
 cat docs_body.html | sed 's/<code/<pre><code class="hljs"/g' | sed 's/<\/code>/<\/code><\/pre>/g' >> docs.html
 cat docs_footer.html >> docs.html
 
-cp docs.html  ../website
+cp docs.html  ../
